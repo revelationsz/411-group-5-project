aed05dbcee0a50e3abfce62223b9400f4631a4e4
sooryu22 <sooryu22@gmail.com>
2021-11-13 11:17:57 -0500

    Changed weatehr.py

--- weather.py
+++ weather.py
@@ -3,35 +3,38 @@ from flask import Flask, render_template, request
 from tkinter import *
 import math
 
-
 city_name = "Boston,US"
 
 api_key = "950143afcefc9332553b25a26396132e"
 
+
 def get_weather(api_key, city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
-
     response = requests.get(url).json()
 
-    temp = response['main']['temp']  # calvin
-    # Convert to ferenheit and rounds up
+    temp = response['main']['temp']
+
+    # converts Kelvin to Fahrenheit and rounds up
     temp = math.floor((temp * 1.8) - 459.67)
 
-    feels_like = response['main']['feels_like']  # calvin
-    # Convert to ferenheit and rounds up
-    feels_like = math.floor((feels_like * 1.8) - 459.67)
-
     humidity = response['main']['humidity']
+
+    # response['weather'][0] = {'id': 801, 'main': 'Clouds', 'description': 'few clouds', 'icon': '02d'}
+    description = response['weather'][0]['main']
+    description_specific = response['weather'][0]['description']
+
     return {
         'temp': temp,
-        'feels_like': feels_like,
-        'humidity': humidity
+        'humidity': humidity,
+        'description': description,
+        'description_specific': description_specific
     }
 
 
 weather = get_weather(api_key, city_name)
 
+print(weather)
 print(weather['temp'])
-print(weather['feels_like'])
 print(weather['humidity'])
-
+print(weather['description'])
+print(weather['description_specific'])
