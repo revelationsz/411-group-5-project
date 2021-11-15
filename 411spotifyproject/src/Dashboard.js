7b8054d9e4233e7e10c26436279579c2834383ef
revelationsz <68308751+revelationsz@users.noreply.github.com>
2021-11-15 09:39:11 -0500

    Gets location, and finds playlists on it
    No more search bar, now gets location and then after you click the button brings up playlists based on the city, next to be used with weather

--- 411spotifyproject/src/Dashboard.js
+++ 411spotifyproject/src/Dashboard.js
@@ -10,6 +10,8 @@ const spotifyApi = new SpotifyWebApi({
     clientId: 'b2cde67bdf9c48da9b51ef5ad9cb6404'
 })
 
+const OpenCage = 'ee2cb42391a0428980f82186d9efe4a8'
+
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code)
     const [search, setSearch] = useState("")
@@ -17,14 +19,33 @@ export default function Dashboard({ code }) {
     const [playingTrack, setPlayingTrack] = useState()
     const [lyrics, setLyrics] = useState("")
     const [playingPlaylist, setPlayList] = useState()
+    const [Location, setLocation] = useState("")
      
-    
 
-    function chooseTrack(track) {
-        setPlayList(track)
-        setSearch("")
-        setLyrics("")
-    }
+
+    const successfulLookup = position => { //gets location from long and lat
+        
+        const {latitude, longitude} = position.coords;
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=ee2cb42391a0428980f82186d9efe4a8`)
+            .then(re => re.json())
+            .then(data => {
+                console.log(data.results[0].components)
+                setLocation(
+                    data.results[0].components.city
+                )                        
+             })
+         }
+        
+        function chooseTrack(track) {
+            setPlayList(track)
+            setSearch("")
+            setLyrics("")
+        }
+        
+        useEffect(() => { //gets loc in form of long and lat 
+                window.navigator.geolocation.getCurrentPosition(successfulLookup, console.log)
+        },[])
+
 
     // useEffect(() => {
     //     if(!playingPlaylist) return
@@ -51,40 +72,36 @@ export default function Dashboard({ code }) {
       
         spotifyApi.searchPlaylists(search).then(res => {
            if(cancel) return
-            
            console.log('Found playlists are', res);
        
             setSearchResults(
                   res.body.playlists.items.map(playlist => {
                     
                    return {
-                        description: playlist.description,
+                       description: playlist.description,
                        title: playlist.name,
                        uri: playlist.uri,
                        albumUrl: playlist.images[0].url,
                    }})
-                   
             )
             },function(err) {
                 console.log('Something went wrong!', err);
          });
-
         
         return () => cancel = true
     },[search, accessToken])
-
-  
 
     return (
         <Container className="d-flex flex-column py-2" style={{ 
             height: "100vh"
         }}>
-           <Form.Control 
+           {/* <Form.Control 
             type="search" 
             placeholder="Search Songs/Artists" 
             value={search}
-            onChange={e => setSearch(e.target.value)}
-           />
+            onChange={e => setSearch(Location)}
+           /> */}
+           <button onClick={e => setSearch(Location)}> Search </button>
            <div className="flex-grow-1 my-2" style={{overflowY: "auto"}}>
                {searchResults.map(playlist => (
                    <TrackSearchResult 
